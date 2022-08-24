044c67bf23aa6087be4f927b03534c9b152832c2
Florian <trafector67890@gmail.com>
2022-08-24 15:33:42 +0200

    cc

---
+++ proxycpp/CMakeFiles/proxycpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/soapDeviceBindingProxy.cpp" "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/CMakeFiles/proxycpp.dir/soapDeviceBindingProxy.cpp.o"
+  "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/soapMediaBindingProxy.cpp" "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/CMakeFiles/proxycpp.dir/soapMediaBindingProxy.cpp.o"
+  "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/soapPTZBindingProxy.cpp" "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/CMakeFiles/proxycpp.dir/soapPTZBindingProxy.cpp.o"
+  "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/soapPullPointSubscriptionBindingProxy.cpp" "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/CMakeFiles/proxycpp.dir/soapPullPointSubscriptionBindingProxy.cpp.o"
+  "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/soapRemoteDiscoveryBindingProxy.cpp" "/home/stagiaire2/Documents/onvif-updated/VersionDemo1/gsoapONVIF/proxycpp/CMakeFiles/proxycpp.dir/soapRemoteDiscoveryBindingProxy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  ".."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proxycpp/CMakeFiles/proxycpp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proxycpp.dir/soapDeviceBindingProxy.cpp.o"
+  "CMakeFiles/proxycpp.dir/soapMediaBindingProxy.cpp.o"
+  "CMakeFiles/proxycpp.dir/soapPTZBindingProxy.cpp.o"
+  "CMakeFiles/proxycpp.dir/soapPullPointSubscriptionBindingProxy.cpp.o"
+  "CMakeFiles/proxycpp.dir/soapRemoteDiscoveryBindingProxy.cpp.o"
+  "libproxycpp.a"
+  "libproxycpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proxycpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
